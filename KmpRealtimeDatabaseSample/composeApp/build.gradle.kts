b2fe8c9bd26c84bccd1266e9681f076479f28e39
seabat <ryouta.nakasnishi.86@gmail.com>
2025-03-15 20:25:23 +0900

    1.0.0(1) -> 1.1.0(2)

--- KmpRealtimeDatabaseSample/composeApp/build.gradle.kts
+++ KmpRealtimeDatabaseSample/composeApp/build.gradle.kts
@@ -81,8 +81,8 @@ android {
         applicationId = "dev.seabat.kmp.rtdb"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1.0"
     }
     packaging {
         resources {
