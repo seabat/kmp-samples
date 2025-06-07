b892f1e3f8e7f45283a3d100f3a4daca51f87ea1
seabat <ryouta.nakasnishi.86@gmail.com>
2025-06-07 23:14:40 +0900

    メインとサブのKoinModuleのリストの結合指定からmodules()にセットする

--- KmpMultiModuleWithSubmodule/shared/src/commonMain/kotlin/dev/seabat/kmp/withsubmodule/shared/di/CommonModule.kt
+++ KmpMultiModuleWithSubmodule/shared/src/commonMain/kotlin/dev/seabat/kmp/withsubmodule/shared/di/CommonModule.kt
@@ -1,43 +1,25 @@
 package dev.seabat.kmp.withsubmodule.shared.di
 
-import dev.seabat.kmp.tutorial.shared.di.useCaseModule as subUseCaseModule
-import dev.seabat.kmp.tutorial.shared.di.viewModelModule as subViewModelModule
-import dev.seabat.kmp.tutorial.shared.di.repositoryModule as subRepositoryModule
-import dev.seabat.kmp.tutorial.shared.di.platformModule as subPlatformModule
-import dev.seabat.kmp.withsubmodule.shared.di.useCaseModule as mainUseCaseModule
-import dev.seabat.kmp.withsubmodule.shared.di.viewModelModule as mainViewModelModule
-import dev.seabat.kmp.withsubmodule.shared.di.repositoryModule as mainRepositoryModule
-import dev.seabat.kmp.withsubmodule.shared.di.platformModule as mainPlatformModule
+import dev.seabat.kmp.tutorial.shared.di.koinModules as subKoinModules
 import org.koin.core.context.startKoin
 import org.koin.dsl.KoinAppDeclaration
+
+val koinModules = listOf(
+    viewModelModule,
+    useCaseModule,
+    repositoryModule,
+    platformModule
+)
 
 // for Android
 fun initAndroidKoin(appDeclaration: KoinAppDeclaration) = startKoin {
     appDeclaration()
-    modules(
-        mainUseCaseModule,
-        mainViewModelModule,
-        mainRepositoryModule,
-        mainPlatformModule,
-        subUseCaseModule,
-        subViewModelModule,
-        subRepositoryModule,
-        subPlatformModule
-    )
+    modules(koinModules + subKoinModules)
 }
 
 // for Compose Preview
 fun initComposePreviewKoin() = startKoin {
-    modules(
-        mainUseCaseModule,
-        mainViewModelModule,
-        mainRepositoryModule,
-        mainPlatformModule,
-        subUseCaseModule,
-        subViewModelModule,
-        subRepositoryModule,
-        subPlatformModule
-    )
+    modules(koinModules + subKoinModules)
 }
 
 // for iOS
@@ -45,15 +27,6 @@ fun initComposePreviewKoin() = startKoin {
 // CommonModuleKt.doInitKoin()
 fun initIosKoin() {
     startKoin {
-        modules(
-            mainUseCaseModule,
-            mainViewModelModule,
-            mainRepositoryModule,
-            mainPlatformModule,
-            subUseCaseModule,
-            subViewModelModule,
-            subRepositoryModule,
-            subPlatformModule
-        )
+        modules(koinModules + subKoinModules)
     }
 }
--- KmpMultiModuleWithSubmodule/shared/src/commonMain/kotlin/dev/seabat/kmp/withsubmodule/shared/di/KoinHelper.kt
+++ KmpMultiModuleWithSubmodule/shared/src/commonMain/kotlin/dev/seabat/kmp/withsubmodule/shared/di/KoinHelper.kt
@@ -1,7 +1,7 @@
 package dev.seabat.kmp.withsubmodule.shared.di
 
 import dev.seabat.kmp.tutorial.shared.usecase.GrepUseCaseContract
-import dev.seabat.kmp.tutorial.shared.usecase.LoadRocketLaunchInfoUseCaseContract
+import dev.seabat.kmp.withsubmodule.shared.usecase.LoadRocketLaunchInfoUseCaseContract
 import dev.seabat.kmp.tutorial.shared.viewmodel.GreetingSharedViewModel
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
@@ -10,6 +10,11 @@ import org.koin.core.component.inject
  * iOS 用に Koin の inject() を実行するクラス
  */
 class KoinHelper : KoinComponent {
+
+    // NOTE: KmpMultiModuleWithSubmodule の LoadRocketLaunchInfoUseCase を DI する場合は
+    //       dev.seabat.kmp.withsubmodule.shared.usecase の LoadRocketLaunchInfoUseCaseContract を使用する。
+    //       KmpTutorial の LoadRocketLaunchInfoUseCase を DI する場合は
+    //       dev.seabat.kmp.tutorial.shared.usecase の LoadRocketLaunchInfoUseCaseContract を使用する。
     val loadRocketLaunchInfoUseCase: LoadRocketLaunchInfoUseCaseContract by inject()
     val grepUseCase: GrepUseCaseContract by inject()
     val greetingSharedViewModel: GreetingSharedViewModel by inject()
